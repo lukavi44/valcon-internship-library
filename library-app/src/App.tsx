0f2db68e2f80d5341408a6e0c5a975f63216288e
lukavi44 <lukaladisic44@gmail.com>
2023-03-02 17:45:44 +0100

    project setup
    app created, configuration done

---
+++ library-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [se, setSe] = useState('')
+  return (
+    <div className='App'>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
