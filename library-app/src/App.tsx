e71eb9c31f803b6969d3a0f45139ddf836325ef5
lukavi44 <lukaladisic44@gmail.com>
2023-03-09 13:11:19 +0100

    review fix

--- library-app/src/App.tsx
+++ library-app/src/App.tsx
@@ -2,18 +2,13 @@ import Homepage from './components/pages/Homepage/Homepage'
 import styles from './App.module.css'
 import { Route, Routes } from 'react-router-dom'
 import Login from './components/pages/Login/Login'
-import { useState } from 'react'
 
 function App() {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [accessToken, setAccessToken] = useState(localStorage.getItem('accessToken')) // it will be used for PrivateRoutes when implemented
-
   return (
     <div className={styles['app-wrapper']}>
-
       <Routes>
         <Route path='/' element={<Homepage />} />
-        <Route path='login' element={<Login setAccessToken={setAccessToken} />} />
+        <Route path='login' element={<Login />} />
       </Routes>
     </div>
   )
